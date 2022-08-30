616d0193ff023c4adf660c76944f27aaee3b25fc
chenyanfei <15873624283@163.com>
2022-08-30 15:01:19 +0800

    feat: cache page

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # my_vue_ssr
 
 ### 一个 Vue SSR 的基本实现
-- 构建配置
-- 路由
-- 状态管理和数据预获取
+- [x] 构建配置
+- [x] 路由
+- [x] 状态管理和数据预获取
+- [x] 页面缓存
+- [ ] 开发模式
 
-### node v12.22.12
+### 其他
+- node v12.22.12
 - pnpm install
 - pnpm run build
 - node server.js
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const LRU = require('lru-cache')
 const { createBundleRenderer } = require('vue-server-renderer')
 const serverBundle = require('./dist/vue-ssr-server-bundle.json')
 const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+
 const server = express()
+
+const microCache = new LRU({
+  max: 100,
+  ttl: 5000, // 重要提示：条目在 5 秒后过期。
+})
+const isCacheable = req => {
+  // 实现逻辑为，检查请求是否是用户特定(user-specific)。
+  // 只有非用户特定 (non-user-specific) 页面才会缓存
+  if (req.url === '/') {
+    return true
+  } else {
+    return false
+  }
+}
+
 const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false,
   template: fs.readFileSync('./index.template.html', 'utf-8'),
@@ -14,6 +31,14 @@ const renderer = createBundleRenderer(serverBundle, {
 server.use('/dist', express.static(path.resolve(__dirname, './dist')))
 
 server.get('*', (req, res) => {
+  const cacheable = isCacheable(req)
+  if (cacheable) {
+    const hit = microCache.get(req.url)
+    if (hit) {
+      return res.end(hit)
+    }
+  }
+
   const context = { url: req.url }
   renderer.renderToString(context, (err, html) => {
     if (err) {
@@ -23,6 +48,9 @@ server.get('*', (req, res) => {
         res.status(500).end('Internal Server Error')
       }
     } else {
+      if (cacheable) {
+        microCache.set(req.url, html)
+      }
       res.end(html)
     }
   })
